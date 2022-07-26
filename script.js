537a0cff00231ef3beb22f88120ccf3797ed998c
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-07-26 12:59:48 -0400

    Add onmouseover and onmouseout for my skills

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const h2 = document.createElement("h2");
+/*const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 /* document.querySelector("body").appendChild(h2); "This goes into the DOM" */
+
+
+function changeBackground() {
+    const text = document.getElementById("my_Skills").style
+    text.backgroundColor= "white" 
+}
+
+function changeBackground2() {
+    const text = document.getElementById("my_Skills2").style
+    text.backgroundColor= "white" 
+}
+
+function backToNormal() {
+    const text = document.getElementById("my_Skills").style
+    text.backgroundColor= "" 
+}
+
+function backToNormal2() {
+    const text = document.getElementById("my_Skills2").style
+    text.backgroundColor= "" 
+}
